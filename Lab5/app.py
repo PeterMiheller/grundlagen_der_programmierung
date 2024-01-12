19f70397e2a4d4b16583aa54402cb6938b9243c3
Peter Miheller <peti.miheller@gmail.com>
2024-01-12 12:05:20 +0200

    new_commit

---
+++ Lab5/app.py
@@ -0,0 +1,15 @@
+from UI.console import Console
+from Tests.test import *
+
+
+def main():
+    my_ui = Console()
+    my_ui.run()
+
+
+main()
+t1 = Tests()
+t1.test_add_cooked_dish()
+t1.test_find_customer_name()
+t1.test_find_customer_adress()
+t1.test_aktualisierung()
